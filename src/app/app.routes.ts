8755810f7264f6ac2afb18836dfb71942d691624
namast0601 <108456121+namast0601@users.noreply.github.com>
2026-01-08 20:54:24 +0700

    Add page: introduction, knowledge, contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { KnowledgeComponent } from './pages/knowledge/knowledge.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'knowledge', component: KnowledgeComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: '**', redirectTo: '' }
